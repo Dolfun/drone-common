c841228069ef2a96db835aa534148eba5899adce
Dolfun <abhinav150104@gmail.com>
2024-01-14 01:52:04 +0530

    Change bytes to use size_t instead of object type

--- include/Serialization.hpp
+++ include/Serialization.hpp
@@ -8,18 +8,18 @@ concept Serializable =
   std::is_standard_layout_v<T> &&
   std::is_trivial_v<T>;
 
-template <Serializable T>
-using bytes = std::array<std::byte, sizeof(T)>;
+template <std::size_t N>
+using bytes = std::array<std::byte, N>;
 
 template <Serializable T>
-auto serialize(const T& val) -> bytes<T> {
-  bytes<T> serialized {};
+auto serialize(const T& val) -> bytes<sizeof(T)> {
+  bytes<sizeof(T)> serialized {};
   std::memcpy(static_cast<void*>(serialized.data()), static_cast<const void*>(&val), sizeof(T));
   return serialized;
 }
 
 template <Serializable T>
-auto unserialize(bytes<T> serialized) -> T {
+auto unserialize(bytes<sizeof(T)> serialized) -> T {
   T val;
   std::memcpy(static_cast<void*>(&val), static_cast<const void*>(serialized.data()), sizeof(T));
   return val;
